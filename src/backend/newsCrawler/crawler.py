38b45d5efec514d4df578aa079b767b8eaba8dc4
Strompcrab <euanoneill@yahoo.co.uk>
2020-10-27 13:04:06 +0000

    Newscrawler now stores data in local txt file - to be used as temp solution

--- src/backend/newsCrawler/crawler.py
+++ src/backend/newsCrawler/crawler.py
@@ -4,42 +4,23 @@ from datetime import datetime
 from newspaper import Article
 import feedparser as fp
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="HotOrNot",
-    user="postgres",
-    password="2206"
-)
+#conn = psycopg2.connect(
+#    host="localhost",
+#    database="HotOrNot",
+#    user="postgres",
+#    password=""
+#)
 
-sources = {
-    "WSJ_Markets": {
-        "rss": "https://feeds.a.dj.com/rss/RSSMarketsMain.xml"
-    },
-    "WSJ_Technology":{
-        "rss": "https://feeds.a.dj.com/rss/RSSWSJD.xml"
-    },
-    "WSJ_US_business":{
-        "rss": "https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml"
-    },
-    "TIME_business":{
-        "rss": "http://feeds.feedburner.com/time/business"
-    },
-    "BBC_business":{
-        "rss": "https://feeds.bbci.co.uk/news/business/rss.xml"
-    },
-    "CNBC":{
-        "rss": "https://www.cnbc.com/id/19746125/device/rss/rss.xml"
-    },
-    "FinancialTimes":{
-        "rss": "https://www.ft.com/?format=rss"
-    },
-    "FortuneRssFeed":{
-        "rss": "http://fortune.com/feed"
-    },
-    "Economic_Times":{
-        "rss": "https://economictimes.indiatimes.com/rssfeedsdefault.cms"
-    }
-}
+sources = {}
+with open('sources.json') as json_file:
+    sources = json.load(json_file)
+
+titles = []
+with open('data.txt') as file:
+    for line in file:
+        j = json.loads(line)
+        for title in j.keys():
+            titles.append(title)
 
 
 # Gets list of current articles for specified source
@@ -57,22 +38,37 @@ def get_articles(articles_source):
 
 
 # Checks if article is already in the database
+#def check(article_title):
+#    cur = conn.cursor()
+#    cur.execute('''SELECT * FROM articles WHERE articles.id = %s ''', (article_title,))
+#    ver = cur.fetchone()
+#    if ver is None:
+#        return True
+#    else:
+#        return False
+#
+#
+# Creates new entry for article in the database, not being uses during this version
+# def upload(article_title,article_data):
+#    cur = conn.cursor()
+#    date = time = datetime.now()
+#    cur.execute('''INSERT INTO articles(id,data,date,time) VALUES(%s,%s,%s,%s)''', (article_title, json.dumps(article_data), date, time))
+#    conn.commit()
+
+
 def check(article_title):
-    cur = conn.cursor()
-    cur.execute('''SELECT * FROM articles WHERE articles.id = %s ''', (article_title,))
-    ver = cur.fetchone()
-    if ver is None:
-        return True
-    else:
+    if article_title in titles:
         return False
+    else:
+        return True
 
 
-# Creates new entry for article in the database
-def upload(article_title,article_data):
-    cur = conn.cursor()
-    date = time = datetime.now()
-    cur.execute('''INSERT INTO articles(id,data,date,time) VALUES(%s,%s,%s,%s)''', (article_title, json.dumps(article_data), date, time))
-    conn.commit()
+def output(article_title, article_data):
+
+    j = {article_title: [article_data, datetime.now().strftime("%m/%d/%Y, %H:%M:%S")]}
+
+    with open("data.txt", "a") as file:
+        file.write(json.dumps(j) + '\n')
 
 
 if __name__ == '__main__':
@@ -86,5 +82,5 @@ if __name__ == '__main__':
                 title = article[0]
                 if check(title):
                     print("Uploading to database...")
-                    upload(title, article)
+                    output(title, article)
         print("Round Complete")
