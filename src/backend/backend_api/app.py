772504fedcd0c0089d9d69347d0181940de273c8
Strompcrab <euanoneill@yahoo.co.uk>
2020-11-13 12:18:25 +0000

    Created basic api calls for companies, sources, articles and sentences

--- src/backend/backend_api/app.py
+++ src/backend/backend_api/app.py
@@ -4,8 +4,106 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def root():
+    return 'Hot or Not api'
+
+
+# <!---- Source calls ----!> #
+
+@app.route('/sources', methods=['GET'])
+def get_sources():
+    return 'gets Sources'
+
+
+@app.route('/sources', methods=['POST'])
+def add_source():
+    return 'adds source'
+
+
+@app.route('/sources/<source_id>', methods=['DELETE'])
+def delete_source(source_id):
+    return source_id
+
+
+# <!---- Article calls ----!> #
+
+@app.route('/article', methods=['POST'])
+def add_article():
+    return 'adds article'
+
+
+@app.route('/sources', methods=['PUT'])
+def update_article():
+    return 'updates article'
+
+
+@app.route('/article/<article_id>', methods=['GET'])
+def get_article(article_id):
+    return article_id
+
+
+@app.route('/article/<article_id>', methods=['DELETE'])
+def delete_article(article_id):
+    return article_id
+
+
+@app.route('/article/<article_id>/context', methods=['PUT'])
+def update_article_context(article_id):
+    return article_id
+
+
+@app.route('/article/findByStatus', methods=['GET'])
+def find_article_by_status():
+    return 'finds article by status'
+
+
+# <!---- Sentence calls ----!> #
+
+@app.route('/sentence', methods=['POST'])
+def add_sentence():
+    return 'adds sentence'
+
+
+@app.route('/sentence/findByStatus', methods=['GET'])
+def find_sentence_by_status():
+    return 'finds sentence by status'
+
+
+@app.route('/sentence/<sentence_id>', methods=['DELETE'])
+def delete_sentence(sentence_id):
+    return sentence_id
+
+
+@app.route('/sentence/<sentence_id>/context', methods=['POST'])
+def update_sentence_context(sentence_id):
+    return sentence_id
+
+
+@app.route('/sentence/<sentence_id>/sentiment', methods=['POST'])
+def update_sentence_sentiment(sentence_id):
+    return sentence_id
+
+
+# <!---- Company calls ----!> #
+
+@app.route('/company', methods=['POST'])
+def add_company():
+    return 'adds company'
+
+
+@app.route('/company/<stock_code>', methods=['DELETE'])
+def delete_articles(stock_code):
+    return stock_code
+
+
+@app.route('/company/<stock_code>/articles', methods=['GET'])
+def get_company_articles(stock_code):
+    return stock_code
+
+
+@app.route('/company/<stock_code>/sentences', methods=['POST'])
+def get_company_sentences(stock_code):
+    return stock_code
 
 
 if __name__ == '__main__':
