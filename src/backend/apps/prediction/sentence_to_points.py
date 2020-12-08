1c48d8d25b018dbb7b87e2c6d96b9a72a00b82fe
Strompcrab <euanoneill@yahoo.co.uk>
2020-12-08 13:27:27 +0000

    Small change

--- src/backend/apps/prediction/sentence_to_points.py
+++ src/backend/apps/prediction/sentence_to_points.py
@@ -1,5 +1,4 @@
 # Used for turning sentences in HotOrNot databse into data points in time series database
-import json
 import requests
 import stock_data
 
@@ -28,22 +27,49 @@ def get_points():
 def sentence_to_point(sentence_json):
     url = baseurl + "/points"
     date_time = sentence_json['date'] + " " + sentence_json['time']
+    data = stock_data.date_at_date(sentence_json['context'], sentence_json['date'] + " " + sentence_json['time'])
     payload = {"time": date_time,
                "company_id": sentence_json['context'],
                "sentiment": sentence_json['sentiment'],
-               "sentence_id": sentence_json['id']}
+               "sentence_id": sentence_json['id'],
+               "open": data['Open'],
+               "high": data['High'],
+               "low": data['Low'],
+               "close": data['Close'],
+               "volume": data['Volume'], }
     r = requests.post(url, json=payload)
 
 
-def update_price(point_id):
-    pass
+# Update stock data of point
+def update_price(point):
+    url = baseurl + "/points/price"
+    data = stock_data.date_at_date(point['stock_code'], point['date_time'])
+    if data.empty:
+        print("Something wrong with stock code: " + point['stock_code'])
+    else:
+        payload = {"id": point['id'],
+                   "open": data['Open'],
+                   "high": data['High'],
+                   "low": data['Low'],
+                   "close": data['Close'],
+                   "volume": data['Volume'], }
+        r = requests.put(url,json=payload)
+        print("point updated")
 
 
 if __name__ == '__main__':
-    get_points()
+    # If need to updated existing point data
+    # for point in get_points():
+    #    if point['open'] == None:
+    #        update_price(point)
+    #        print("Point updated")
 
-    #while(True):
-    #    for sentence in get_sentences():
-    #        sentence_to_point(sentence)
-    #        print("point created")
+    while(True):
+        for sentence in get_sentences():
+            sentence_to_point(sentence)
+            print("Point created")
+
+
+
+
 
