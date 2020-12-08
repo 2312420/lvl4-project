dfec574ef5c53693a74d303a05673f5a94776756
Strompcrab <euanoneill@yahoo.co.uk>
2020-12-08 10:35:22 +0000

    Created date-time method

--- src/backend/apps/prediction/stock_data.py
+++ src/backend/apps/prediction/stock_data.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+from datetime import datetime
 
 
 # Data format is Year-Month-day
@@ -8,5 +9,8 @@ def get_stock_data(stock_code, start_date, end_date, peroid):
     return stock_df
 
 
-
+# Takes date and time stirng and turns it into date time object
+def to_datetime(date_string):
+    date_time = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+    return date_time
 
