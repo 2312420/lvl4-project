8729fec910ee34bdfb52726ae36d729fae238416
Strompcrab <euanoneill@yahoo.co.uk>
2020-12-09 16:36:08 +0000

    Setup basic company page that connect from homepage

--- src/frontend/hotornot/urls.py
+++ src/frontend/hotornot/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:stock_code>', views.company_page, name='company_page')
 ]
--- src/frontend/hotornot/views.py
+++ src/frontend/hotornot/views.py
@@ -7,3 +7,9 @@ def index(request):
     companies = Company.objects.all()
     print(companies)
     return render(request, 'home.html', context={'companies': companies})
+
+
+def company_page(request, stock_code):
+    company = Company.objects.get(stock_code=stock_code)
+
+    return render(request, 'company.html', context={'company': company})
