3434201df5b8dcb537e6e91c0a404cb6a8a1971d
Strompcrab <euanoneill@yahoo.co.uk>
2020-12-10 16:27:23 +0000

    Improved sentiment imputor and prediction

--- src/backend/apps/prediction/models.py
+++ src/backend/apps/prediction/models.py
@@ -2,6 +2,11 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.impute import KNNImputer
+from sklearn.impute import SimpleImputer
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime
@@ -27,22 +32,56 @@ def linear_regression(df, target_feature, future_days):
     x_train = data[['sentiment', 'day', 'day_year', 'day_month', 'day_week', 'day_hour', 'day_minute', 'day_dayofweek']]
     y_train = data['close']
 
+    scaler = StandardScaler()
+    x_scale = scaler.fit_transform(x_train, y_train)
+    x_future = future_sentiment_regression(x_train, 5)
 
+    model = LinearRegression().fit(x_train, y_train)
+    preds = model.predict(x_future)
+    x_future['predictions'] = preds
 
-    x_future = future_sentiment_regression(data, 5)
-
-    #pipe = make_pipeline(StandardScaler(), LinearRegression())
-    #pipe.fit(x_train, y_train)
-    #preds = pipe.predict(x_future)
-    #x_future['predictions'] = preds
-
-    print(data[['sentiment']])
-    print(x_future[['sentiment']])
-
+    # Plot graph (for testing)
     #plt.plot(data['close'])
-    #plt.plot(x_future['predictions'])
+    #plt.plot(x_future['predictions'], color='green', alpha=0.4)
     #plt.xticks(fontsize=5)
     #plt.show()
+
+    return x_future
+
+
+# Used to fill in missing sentiment data
+def past_sentiment_regression(df):
+    train = df.copy()
+    x_train = train.drop(["sentiment", "time"], axis=1)
+    y_train = train['sentiment']
+    model = LinearRegression()
+    model.fit(x_train, y_train)
+    return model
+
+
+# Used to predict future sentiment
+def future_sentiment_regression(df, future_days):
+    # Create future dataframe
+    now = datetime.now().replace(microsecond=0)
+    future_df = pd.DataFrame({'time': [now]})
+
+    for i in range(future_days - 1):
+        future_df = future_df.append({'time': now + timedelta(i), 'sentiment': None}, ignore_index=True)
+
+    future_df = expand_time(future_df)
+    future_df.index = future_df['time']
+    future_df = future_df.drop(['time'], axis=1)
+
+    future_df = pd.concat([df, future_df])
+
+    #imp = SimpleImputer(missing_values=np.nan, strategy='median')
+    #future_df['sentiment'] = imp.fit_transform(future_df)[:,0]
+
+    imp = IterativeImputer(max_iter=10, random_state=0)
+    future_df['sentiment'] = imp.fit_transform(future_df)[:,0]
+
+    return future_df
+
 
 # Used for testing data on self
 def test_model(df, split, target_feature):
@@ -73,43 +112,3 @@ def test_model(df, split, target_feature):
     plt.plot(valid[['close', 'predictions']])
     plt.xticks(fontsize=5)
     plt.show()
-
-
-# Used to fill in missing sentiment data
-def past_sentiment_regression(df):
-    train = df.copy()
-    x_train = train.drop(["sentiment", "time"], axis=1)
-    y_train = train['sentiment']
-    model = LinearRegression()
-    model.fit(x_train, y_train)
-    return model
-
-
-# Used to predict future sentiment
-def future_sentiment_regression(df, future_days):
-    # Create future sentiment model
-
-    x_train = df[['day_year', 'day', 'day_month', 'day_week', 'day_hour', 'day_minute', 'day_dayofweek']]
-    y_train = df['sentiment']
-
-    scaler = StandardScaler()
-    x_scale = scaler.fit_transform(x_train, y_train)
-
-    model = LinearRegression()
-    model.fit(x_scale, y_train)
-
-    # Create future dataframe
-    now = datetime.now()
-    future_df = pd.DataFrame({'time': [now]})
-
-    for i in range(future_days - 1):
-        future_df = future_df.append({'time': now + timedelta(i)}, ignore_index=True)
-
-    future_df = expand_time(future_df)
-    future_df.index = future_df['time']
-    future_df = future_df.drop(['time'], axis=1)
-
-    # Predict future sentiment
-    preds = model.predict(scaler.fit_transform(future_df))
-    future_df['sentiment'] = preds
-    return future_df
