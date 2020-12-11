c817810ddda2a5306e568093c24af0da6a2a5057
Strompcrab <euanoneill@yahoo.co.uk>
2020-12-11 15:02:54 +0000

    split company results into sepearte html

--- src/frontend/hotornot/views.py
+++ src/frontend/hotornot/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from hotornot.models import Company
+from django.template.loader import render_to_string
+
 
 
 def index(request):
-    companies = Company.objects.all()
-    print(companies)
+    ctx = {}
+    url_parameter = request.GET.get("f")
+
+    if url_parameter:
+        companies = Company.objects.filter(short_hand__icontains=url_parameter)
+    else:
+        companies = Company.objects.all()
+
+    if request.is_ajax():
+        html = render_to_string(
+
+        )
+
     return render(request, 'home.html', context={'companies': companies})
 
 
 def company_page(request, stock_code):
     company = Company.objects.get(stock_code=stock_code)
     return render(request, 'company.html', context={'company': company})
+
+
