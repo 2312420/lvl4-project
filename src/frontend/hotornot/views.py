6af4808a767e0217f3ef6b171b0672be215e8fd4
Strompcrab <euanoneill@yahoo.co.uk>
2020-12-14 15:01:45 +0000

    Company page now shows current stock price

--- src/frontend/hotornot/views.py
+++ src/frontend/hotornot/views.py
@@ -8,7 +8,8 @@ from django.http import JsonResponse
 from datetime import datetime
 from datetime import timedelta
 import yfinance as yf
-import json
+from yahoo_fin import stock_info as si
+
 
 # Home page view
 def index(request):
@@ -34,24 +35,37 @@ def index(request):
     return render(request, 'home.html', context={'companies': companies})
 
 
-
-import pickle
-
 # Company page view
 def company_page(request, stock_code):
     company = Company.objects.get(stock_code=stock_code)
 
+    current_price = round(si.get_live_price(stock_code), 2)
+    print(current_price)
+
+    if request.is_ajax():
+        print("!")
+        html = render_to_string(
+            template_name="company-price-ticker.html",
+            context={'current_data': {'price': current_price}}
+        )
+        data_dict = {"html_from_view": html}
+        return JsonResponse(data=data_dict, safe=False)
+
+    ## Stock Information
     stock_data = yf.Ticker(stock_code)
     stock_df = stock_data.history(start=(datetime.now() - timedelta(days=20)), end=datetime.now())
 
-    labels = []
+    close_labels = []
     for item in stock_df.index.to_list():
-        labels.append(datetime.strptime(str(item), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S'))
+        close_labels.append(datetime.strptime(str(item), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S'))
+    close_prices = stock_df['Close'].to_list()
 
-    close = stock_df['Close'].to_list()
 
-    test = ['1','2']
-    return render(request, 'company.html', context={'company': company, 'labels': labels, 'close': close, 'data': [1, 2, 3, 4, 5]})
+    return render(request, 'company.html', context={'company': company,
+                                                    'close_data':   {'labels': close_labels,
+                                                                     'prices': close_prices},
+                                                    'current_data': {'price': current_price }
+                                                    })
 
 
 def redirect(request):
