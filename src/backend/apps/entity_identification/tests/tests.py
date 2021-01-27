a8327d4f00ba3e6ec831fd6b109ee7c5b51395ba
Strompcrab <euanoneill@yahoo.co.uk>
2021-01-27 14:35:04 +0000

    test can now find matching orgs

--- src/backend/apps/entity_identification/tests/tests.py
+++ src/backend/apps/entity_identification/tests/tests.py
@@ -1,5 +1,7 @@
 from sentence_ident import analyse
 import csv
+from fuzzywuzzy import fuzz
+from sentence_ident import analyse
 
 #https://www.kaggle.com/rohitr4307/ner-dataset
 
@@ -15,21 +17,49 @@ def convert(lis_str):
 with open('data_sets/NER_Dataset.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+
+    # Metrics from http://www.davidsbatista.net/blog/2018/05/09/Named_Entity_Evaluation/
+    truePositive = 0
+    falseNegative = 0
+    falsePositive = 0
+
+    cor = 0
+    inc = 0
+    par = 0
+    mis = 0
+    spu = 0
+
     for row in csv_reader:
         if line_count != 0:
-            text = convert(row[1])
+            text = row[1].replace("['", '').replace("']", '').replace("', '", ' ')
+            text_lis = convert(row[1])
             objs = convert(row[3])
 
-            orgs = []
-            for i in range(len(text)):
+            valid_orgs = []
+            for i in range(len(text_lis)):
                 if objs[i] == "B-org":
-                    orgs.append(text[i])
+                    valid_orgs.append(text_lis[i])
 
-            print(orgs)
+            print("valid:" + str(valid_orgs))
+            our_orgs = analyse(text)
+            print(our_orgs)
+
+            for item in valid_orgs:
+                if item in our_orgs:
+                    print("True Positive")
+                    our_orgs.remove(item)
+                else:
+                    print("False Negative")
+
+            if len(our_orgs) > 0:
+                print(len(our_orgs))
+
+
 
         line_count += 1
 
-        if line_count == 5:
-            break
+
+
+
 
 
