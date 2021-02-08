18c21ce1cdbb59b9baa158d8fc1abeb8dc1b3053
Strompcrab <euanoneill@yahoo.co.uk>
2021-02-08 14:36:47 +0000

    Updated sentiment metrics

--- src/backend/apps/sentiment_analysis/main.py
+++ src/backend/apps/sentiment_analysis/main.py
@@ -1,9 +1,13 @@
 # Imports
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from flask import Flask, request
-import flask
 import json
-import requests
+import spacy
+import en_core_web_sm
+
+import stanza
+
+from textblob import TextBlob
 
 baseurl = "http://127.0.0.1:5000"
 
@@ -12,10 +16,35 @@ app = Flask(__name__)
 
 # Analyses sentiment score of sentence
 # will be replaced with more complex system if time permits
+#def get_sentiment_score(sentence):
+#    sent = SentimentIntensityAnalyzer()
+#    sent_dict = sent.polarity_scores(sentence)
+#    return sent_dict['compound']
+
+
+
+#stanza.download('en', processors='tokenize,sentiment')
+
+#def get_sentiment_score(sentence):
+#    nlp = stanza.Pipeline(lang='en', processors='tokenize, sentiment')
+#    doc = nlp(sentence)
+#    for i, sentence in enumerate(doc.sentences):
+#        print(i, sentence.sentiment)
+
+
+#nlp = spacy.load("en_core_web_sm")
+#
+#def get_sentiment_score(sentence):
+#    doc = nlp(sentence)
+#    token_list = [token for token in doc if not token.is_stop]
+
+#    lemmas = [f"Token: {token}, lemma: {token.lemma_}" for token in token_list ]
+#    print(token_list[1].vector)
+
+
 def get_sentiment_score(sentence):
-    sent = SentimentIntensityAnalyzer()
-    sent_dict = sent.polarity_scores(sentence)
-    return sent_dict['compound']
+    blob = TextBlob(sentence)
+    return blob.sentiment.polarity
 
 
 @app.route('/', methods=['GET'])
@@ -32,3 +61,4 @@ def get_sentence_sentiment():
 
 if __name__ == "__main__":
     app.run(port=5003)
+    #get_sentiment_score("This is Awful, worse and terrible")
--- src/backend/apps/sentiment_analysis/tests/tests.py
+++ src/backend/apps/sentiment_analysis/tests/tests.py
@@ -12,12 +12,38 @@ def run_sentiment(text):
 
 
 def run_test(note):
+
+    now = datetime.now()
+    print(now)
+
     correct = 0
     incorret = 0
 
     false_neutral = 0
     false_positive = 0
     false_negative = 0
+
+    total_pos = 0
+    total_neg = 0
+    total_neu = 0
+
+    acu_pos = 0
+    acu_neg = 0
+    acu_neu = 0
+
+
+    # Pred, Actual
+    pos_pos = 0
+    pos_neg = 0
+    pos_neu = 0
+
+    neg_pos = 0
+    neg_neg = 0
+    neg_neu = 0
+
+    neu_pos = 0
+    neu_neg = 0
+    neu_neu = 0
 
     with open('data_sets/all-data.csv') as f:
         csv_reader = csv.reader(f, delimiter=',')
@@ -28,37 +54,51 @@ def run_test(note):
             real_sentiment = row[1]
             pred_sentiment = run_sentiment(text)
 
-            if real_sentiment == -1:
+            if int(real_sentiment) == -1:
             # Sentiment is negative
+                acu_neg += 1
+
                 if pred_sentiment < 0:
                     correct += 1
+                    neg_neg += 1
                 else:
                     if pred_sentiment == 0:
                         false_neutral += 1
+                        neu_neg += 1
                     else:
                         false_positive += 1
+                        pos_neg += 1
 
-            elif real_sentiment == 1:
+            elif int(real_sentiment) == 1:
             # Sentiment is positive
+                acu_pos += 1
                 if pred_sentiment > 0:
                     correct += 1
+                    pos_pos += 1
                 else:
                     if pred_sentiment == 0:
                         false_neutral += 1
+                        neu_pos += 1
                     else:
                         false_negative += 1
+                        neg_pos += 1
                     incorret += 1
 
             else:
             # Sentiment is neutral
+                acu_neu += 1
                 if -0.3 <= pred_sentiment <= 0.3:
                     correct += 1
+                    neu_neu += 1
                 else:
                     if pred_sentiment < 0:
                         false_negative += 1
+                        neg_neu += 1
                     else:
                         false_positive += 1
+                        pos_neu += 1
                     incorret += 1
+
 
         with open('results/' + now + "(" + note + ").csv", "x") as o:
             csv_writer = csv.writer(o, delimiter=',',)
@@ -66,10 +106,13 @@ def run_test(note):
             csv_writer.writerow(['total sentences', correct + incorret])
             csv_writer.writerow(['correct', correct])
             csv_writer.writerow(['incorrect', incorret])
-            csv_writer.writerow(['false neutral', false_neutral])
-            csv_writer.writerow(['false negative', false_negative])
-            csv_writer.writerow(['false positive', false_positive])
+            csv_writer.writerow("CONFUSION MATRIX")
+            csv_writer.writerow(["-", "-", "Positive", "Negative", "Neutral"])
+            csv_writer.writerow(["-", "-", pos_neg + pos_neu + pos_pos, neg_neg + neg_neu + neg_pos, neu_pos + neu_neg + neu_neu])
+            csv_writer.writerow(["Positive", acu_pos, pos_pos, neg_pos, neu_pos ])
+            csv_writer.writerow(["Negative", acu_neg, pos_neg, neg_neg, neu_neg])
+            csv_writer.writerow(["Neutral", acu_neu, pos_neu, neg_neu, neu_neu])
 
+    print(now, datetime.now())
 
-
-run_test("vaderSentiment")
+run_test("textBlob, new metrics")
