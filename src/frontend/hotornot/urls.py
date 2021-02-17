6a6ea2fd726b189434691798344c8b8775e96195
Strompcrab <euanoneill@yahoo.co.uk>
2021-02-17 10:41:57 +0000

    Fix to urls

--- src/frontend/hotornot/urls.py
+++ src/frontend/hotornot/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/company/<str:stock_code>', views.company_page, name='company_page'),
+    path('company/<str:stock_code>', views.company_page, name='company_page'),
     path('HowItWorks', views.how_it_works, name='how_it_works'),
     path('About', views.about, name='about')
 ]
